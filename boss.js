ba7f591992753d45170aa19fca4adedcfe2eee1f
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-06-15 07:26:47 -0300

    v_93.3

--- boss.js
+++ boss.js
@@ -172,10 +172,10 @@ module.exports = {
 	],
 	AA_BOSS_3 : [
 	    {id: 113, msg: 'Frente - ATRAS STURN'},
-        //{id: 111, msg: '(T) <<-----DERECHO Seguro, SALIR'}, //msgForTanks: 'Left Safe, OUT safe'},
-        //{id: 111, msg: '(T) <<-----DERECHO Seguro, SALIR'}, //msgForTanks: 'Left Safe, OUT safe'}, // rage
-        //{id: 109, msg: 'IZQUIERDO Seguro, ENTRAR----->> (T)'},   //msgForTanks: 'Right Safe, IN safe'},
-        //{id: 109, msg: 'IZQUIERDO Seguro, ENTRAR----->> (T)'},   //msgForTanks: 'Right Safe, IN safe'}, // rage
+        {id: 111, msg: '(T) <<-----DERECHO Seguro, SALIR'}, //msgForTanks: 'Left Safe, OUT safe'},
+        {id: 111, msg: '(T) <<-----DERECHO Seguro, SALIR'}, //msgForTanks: 'Left Safe, OUT safe'}, // rage
+        {id: 109, msg: 'IZQUIERDO Seguro, ENTRAR----->> (T)'},   //msgForTanks: 'Right Safe, IN safe'},
+        {id: 109, msg: 'IZQUIERDO Seguro, ENTRAR----->> (T)'},   //msgForTanks: 'Right Safe, IN safe'}, // rage
 		{id: 310, msg: 'Ola de agua'},
 		{id: 311, msg: 'Ola de agua'},
 		{id: 312, msg: 'Ola de agua'},
@@ -250,10 +250,10 @@ module.exports = {
 		{id: 162, msg: 'Frente - Atras'},
 		{id: 213, msg: 'Cola!!'},
 		{id: 215, msg: 'Cola!!'},
-		//{id: 139, msg: 'IZQUIERDA Segura----->> (T)'}, //151
-		//{id: 150, msg: 'IZQUIERDA Segura----->> (T)'}, //151
-		//{id: 141, msg: '(T) <<-----DERECHO SEGURO'}, //153
-		//{id: 152, msg: '(T) <<-----DERECHO SEGURO'}, //153
+		{id: 139, msg: 'IZQUIERDA Segura----->> (T)'}, //151
+		{id: 150, msg: 'IZQUIERDA Segura----->> (T)'}, //151
+		{id: 141, msg: '(T) <<-----DERECHO SEGURO'}, //153
+		{id: 152, msg: '(T) <<-----DERECHO SEGURO'}, //153
 		{id: 300, msg: 'DESPIERTAR', level_Msg: ['1!', '2!!', '3!!!', '<font color="#FF0000">4!!!!</font>']},
 		{id: 399, msg: 'Despiertar Secundario', level_Msg: ['1!', '<font color="#FF0000">2!!</font>']},
 		{id: 360, msg: 'Explosion !! Explosion!!'}
